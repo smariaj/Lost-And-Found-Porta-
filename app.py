a6b55980a6682ddcfddc9173ff6e5556d9078e17
smariaj <mariajshaikh07@gmail.com>
2025-11-04 23:52:45 +0530

    Final Product

--- app.py
+++ app.py
@@ -521,4 +521,4 @@ if __name__ == '__main__':
     init_db()
     print("Server starting...")
     print("Admin: admin@lostfound.com / Admin123!")
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, port=5000)
